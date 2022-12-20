fde9ed8997f3dfce27d9e5ae1021f0bdf404cfbd
Zeinab <soleimanikia.z@gmail.com>
2022-12-20 09:41:03 +0330

    weather icon added

--- SRC/index.js
+++ SRC/index.js
@@ -20,39 +20,55 @@ let year = now.getUTCFullYear();
 let exactDay = now.getDate();
 let hour = now.getHours();
 let min = now.getMinutes();
-let h5 = document.querySelector("h5");
-h5.innerHTML = `${hour}:${min} ${Day}, ${month} ${exactDay} ${year}`;
+let currentTime = document.querySelector("#date");
+currentTime.innerHTML = `${hour}:${min} ${Day}, ${month} ${exactDay} ${year}`;
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return ` ${hours}:${minutes}`;
+}
 
 function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
+  let temperature = Math.round(response.data.temperature.current);
   let h2 = document.querySelector("#h2Temp");
   h2.innerHTML = temperature;
   let h6 = document.querySelector("h6");
-  h6.innerHTML = response.data.weather[0].main;
-  document.querySelector("h1").innerHTML = response.data.name;
+  h6.innerHTML = response.data.condition.description;
+  let humidityandwind = document.querySelector("#humidityAndwind");
+  humidityandwind.innerHTML =
+    "Humidity: " +
+    response.data.temperature.humidity +
+    "%          Wind: " +
+    Math.round(response.data.wind.speed) +
+    "kmps";
+  document.querySelector("h1").innerHTML = response.data.city;
+  let weatherIcon = document.querySelector("#mainIcon");
+  weatherIcon.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+
+  let timeZone = document.querySelector("#timezone");
+  timeZone.innerHTML = formatDate(response.data.time * 1000);
 }
 
 function cityandTemp(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#enterCity");
   let h1 = document.querySelector("h1");
-  let apiKey = "cc92c7497f4b5296aba156333ba72192";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=cc92c7497f4b5296aba156333ba72192&units=metric`;
+  let apiKey = "e3f5c70f0f06tb1d5a445afb715o7c01";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput.value}&key=e3f5c70f0f06tb1d5a445afb715o7c01&units=metric`;
 
   axios.get(apiUrl).then(showTemperature);
 }
 let cityName = document.querySelector("#typename");
 cityName.addEventListener("submit", cityandTemp);
-
-function getMyLocation(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(showTemperature);
-}
-
-function showLocation(event) {
-  navigator.geolocation.getCurrentPosition(getMyLocation);
-}
-let showcurrentLocation = document.querySelector("#CurrentLocation");
-showcurrentLocation.addEventListener("click", showLocation);
